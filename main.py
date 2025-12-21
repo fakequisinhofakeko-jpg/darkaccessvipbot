362bc4a45ec2ce5d7d2e30b4031acf9d2414a2f6
fakequisinhofakeko-jpg <fakequisinhofakeko@gmail.com>
2025-12-20 22:47:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
 import os
-import asyncio
 from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    ContextTypes
-)
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-GROUP_ID = int(os.environ.get("GROUP_ID"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🔥 Dark Access VIP online.")
+    await update.message.reply_text("🤖 Bot online com sucesso!")
 
-async def status(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("⏳ Status: EM VERIFICAÇÃO")
-
-async def main():
+def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("status", status))
-
-    print("🤖 Bot iniciado com sucesso")
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
